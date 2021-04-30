50c0162adf84998db3257627d0d245465de375ff
Ryosuke Kikuchi <r-kikuchi@dip-net.co.jp>
2021-05-01 08:35:25 +0900

    go-server implement

---
+++ go-server/go.mod
@@ -0,0 +1,12 @@
+module github.com/Yoyo-Kikuchi/grpc-sample/go-server
+
+go 1.15
+
+require (
+	github.com/fullstorydev/grpcurl v1.8.0 // indirect
+	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
+	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
+	google.golang.org/genproto v0.0.0-20210429181445-86c259c2b4ab // indirect
+	google.golang.org/grpc v1.37.0
+	google.golang.org/protobuf v1.26.0
+)
---
+++ go-server/src/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/Yoyo-Kikuchi/grpc-sample/go-server/src/pd"
+	"github.com/Yoyo-Kikuchi/grpc-sample/go-server/src/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	listenPort, err := net.Listen("tcp", "0.0.0.0:19003")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	server := grpc.NewServer()
+	catService := &service.MyCatService{}
+	// 実行したい実処理をseverに登録する
+	reflection.Register(server)
+	pb.RegisterCatServer(server, catService)
+	server.Serve(listenPort)
+}
